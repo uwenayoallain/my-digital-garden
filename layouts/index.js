df4b11a2ecf0d35e9c09ef65e1dc6396f94d02bd
Yarrison allain <uwenayoallain@gmail.com>
2021-07-26 13:26:04 +0200

    making a layout for all mdx files

--- layouts/index.js
+++ layouts/index.js
@@ -1,30 +1,29 @@
-import Head from 'next/head'
 import Link from 'next/link'
-import Header from '../components/blog/Header'
-import Container from '../components/simplifiers/Container';
-import { useCountPageView } from '../hooks/useCountpageviews';
+import Header from '@/components/blog/Header'
+import Container from '@/components/simplifiers/Container';
+import { useCountPageView } from '@/hooks/useCountpageviews';
+import Text from '@/components/blog/Text';
+import Badge from '@/components/blog/Badge';
 
 export default function PostLayout({ children, frontMatter }) {
-    const { title, path } = frontMatter;
-    const views = useCountPageView(path);
+    const { title, slug, readingTime, by, date } = frontMatter;
+    const views = useCountPageView(slug);
     return (
         <Container>
             <div className="my-5">
                 <Header />
             </div>
-            <Head>
-                <title>{title}</title>
-            </Head>
             <main className="prose-xl prose text-left markdown">
                 <div>
                     <h1 className="capitalize">
                         {title}
                     </h1>
-                    <p>{views} views</p>
+                    <Text>{views} views &#8226; {readingTime.text} &#8226; {readingTime.words} words</Text>
+                    <Badge>{by}</Badge>
                 </div>
                 {children}
             </main>
-            <Link href="/blog">
+            <Link href="/">
                 <a className="static">
                     Back
                 </a>
--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,15 +1,37 @@
-import { MDXRemote } from 'next-mdx-remote';
-import PostLayout from '../../layouts/index';
-import Head from "next/Head";
+import { postFilePaths } from '@/utils/mdxUtils'
+import { MDXRemote } from 'next-mdx-remote'
+import dynamic from 'next/dynamic'
+import Head from 'next/head'
+import CustomLink from '@/components/CustomLink'
+import PostLayout from "@/layouts/index"
+import { getFileBySlug } from '@/lib/mdx'
 
-export default function Blog({ mdxSource, frontMatter }) {
+const components = {
+  a: CustomLink,
+  TestComponent: dynamic(() => import('@/components/TestComponent')),
+  Head,
+}
 
+export default function PostPage({ mdxSource, frontMatter }) {
   return (
     <PostLayout frontMatter={frontMatter}>
-      <MDXRemote
-        {...mdxSource}
-        components={Head}
-      />
+      <MDXRemote {...mdxSource} components={components} />
     </PostLayout>
-  );
+  )
+}
+
+export const getStaticProps = async ({ params }) => {
+  const post = await getFileBySlug(params.slug);
+  return { props: { ...post } };
+}
+
+export const getStaticPaths = async () => {
+  const paths = postFilePaths
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    .map((slug) => ({ params: { slug } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
 }
